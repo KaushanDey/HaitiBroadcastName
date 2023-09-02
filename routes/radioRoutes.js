cdf3dda157da1150087d574718521963dad22fb7
KaushanDey <kaushandey2003@yahoo.com>
2023-09-02 23:23:32 +0530

    Tenth Commit

--- routes/radioRoutes.js
+++ routes/radioRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAllRadio, getAllRadioCategories, getAllRadioLocation, getRadioByFilter, postFavoriteStations } from "../controllers/radioController.js";
+import { getAllRadioCategories, getAllRadioLocation, getRadioByFilter, postFavoriteStations } from "../controllers/radioController.js";
 
 const radioRouter = express.Router();
 
-radioRouter.get("/",getAllRadio);
-radioRouter.get("/filters",getRadioByFilter);
+// radioRouter.get("/",getAllRadio);
+radioRouter.get("/",getRadioByFilter);
 radioRouter.get("/locations",getAllRadioLocation);
 radioRouter.get("/categories",getAllRadioCategories);
 radioRouter.post("/post/favorites",postFavoriteStations);
